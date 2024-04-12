6dca27c144fc0cfd32bb9277cbce1c7962817f60
vika5450 <victoria.vyatkina@mail.ru>
2024-04-12 08:34:15 +0300

    Создали ветку, внесли изменения, пуш-коммит

--- laba4.py
+++ laba4.py
@@ -1,18 +1,28 @@
-# Алгоритм
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
+def dfs(graph, start):
+    visited = set()  # Множество посещенных вершин
+    stack = [(start, 0)]  # Стек для обхода, включая длину шага
 
-    print(start)
+    while stack:
+        vertex, distance = stack.pop()  # Извлекаем вершину и длину шага из стека
 
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+        if vertex not in visited:
+            visited.add(vertex)  # Посещаем вершину
+            print(vertex, distance)  # Можно изменить на сохранение в путь
 
+            # Добавляем непосещенные соседние вершины с учетом расстояния в стек
+            neighbors = graph[vertex]
+            for neighbor, step in neighbors:
+                if neighbor not in visited:
+                    stack.append((neighbor, distance + step))
 
-graph = {'0': set(['4', '2']),
-         '1': set(['1', '3']),
-         '2': set(['2', '4'])}
+# Пример графа с длиной
+# шага между вершинами
+graph = {
+    1: [(3, 2)],
+    2: [(4, 1)],
+    3: [],
+    4: [(2, 3)]
+}
 
-dfs(graph, '0'))
+# Запуск обхода в глубину 1
+dfs(graph, 1)
